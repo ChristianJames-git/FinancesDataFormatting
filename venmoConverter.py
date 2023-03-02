e255e9f98b2c69c472ada48b72cf733d5d2fbc81
Christian James <christianjames.public@gmail.com>
2023-03-01 22:41:03 -0800

    Complete Venmo Parser

--- venmoConverter.py
+++ venmoConverter.py
@@ -1,33 +1,53 @@
-# WIP
+# Input:
+#   You paid Eugenio Casta
+#   1d
+#   Heater
+#   - $10.00
+# Output:
+#   2023-02-28 Eugenio Casta Heater -$10.00
 
-from datetime import date
+from datetime import date as datet
+from datetime import timedelta
 
 with open('venmos.txt', 'r') as f:
     lines = f.readlines()
 
 count = 1
-today = date.today()
+transfer = ""
+today = datet.today()
+date = datet.today()
 desc = ""
 amount = ""
 payments = []
 for line in lines:
-    if line.strip() == "":
-        count = 1
-        continue
-    if count == 1:
-        month, day, year = line.split(" ")
-        day = day.replace(',', "")
-        year = year.strip()
-        count = 2
-        continue
-    if count == 2:
-        desc = line.split(" ")
-        count = 3
-        continue
-    if count == 3:
-        amount = "-" + line.strip()[1:]
-        payments.append(f"{month}/{day}/{year} {desc[0]} {amount}")
-        continue
+    line = line.strip("\n")
+    match count:
+        case 1:
+            splitLine = line.split(" ")
+            if line[0] == 'Y':  # case for me paying or chargine
+                desc = f"{splitLine[2]} {splitLine[3]}"
+            elif line[0:12] == "Bank Transfer": #  case for bank transfer
+                desc = f"Transfer Venmo"
+            else:  # case for me being paid or charged
+                desc = f"{splitLine[0]} {splitLine[1]}"
+        case 2:
+            if line[len(line)-1] == 'h':
+                date = today
+            elif line[len(line)-1] == 'd':
+                date = today - timedelta(days=int(line[0:len(line)-1]))  # subtract days
+        case 3:
+            if line[0:18] == "Estimated arrival:":
+                transfer = "-"
+                continue
+            desc = f"{desc} {line}"
+        case 4:
+            line = line.replace(" ", "")
+            amount = f"{transfer}{line}"
+            transfer = ""
+            payments.append(f"{date} {desc} {amount}")
+            count = 0
+    count += 1
+
 payments.reverse()
 for payment in payments:                                                    
     print(payment)
