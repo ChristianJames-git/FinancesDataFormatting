111f4e25a99806d3c44f0bfd6252f24100caab84
Christian James <christianjames.public@gmail.com>
2025-03-08 00:50:55 -0800

    Update schwab converter to use regex

--- Converters/schwabConverter.py
+++ Converters/schwabConverter.py
@@ -1,35 +1,21 @@
+import re
+
 def schwab_to_sheets(lines, acc):
     sheets_lines = []
-    converts_set = set(_schwab_conversions.keys())
+    text = "\n".join(lines)
+    text = text.replace("−", "-")
+    
+    matches = re.findall(r"(\d{2}/\d{2}/\d{4})\t[A-Z]+\t+([^	]+?)\t(\t?)(\$?\d{1,3}(?:,\d{3})*\.\d{2})[\s\S]*?\n?", text)
 
-    for line in reversed(lines):
-        # parts = list(filter(None, line.split("\t")))
-        parts = list(line.strip().split("\t"))
-        date = parts[0]
-        desc = parts[3]
-        if parts[4]:
-            price = "-" + parts[4]
-        else:
-            price = parts[5]
-        key = next((key for key in converts_set if key in desc), None)
-        if key:
-            desc = _schwab_conversions[key](desc)
-        output_string = f"{date}@{desc}@{acc}@{price}"
-        # output.write(f"{date}@{desc}@{acc}@{price}\n")
+    formatted_output = [f"{date}@{description.strip()}@{acc}@{format_amount(amount, tab)}" for date, description, tab, amount in matches]
+    formatted_output.sort()
+
+    for output_string in formatted_output:
         sheets_lines.append(output_string)
+
     return sheets_lines
 
-
-# def schwab_to_sql(lines, acc):
-#     sheets_lines = schwab_to_sheets(lines, acc)
-#     sql_lines = []
-#     for line in sheets_lines:
-#         date, desc, account, price = line.split('@')
-#         category = 'Shopping'
-#         sql_lines.append([date, desc, account, price, category])
-#     return sql_lines
-
-
-_schwab_conversions = {
-    "Amazon": lambda x: "Amazon Prime",
-}
+def format_amount(amount, tab):
+    if not tab:
+        amount = "-" + amount
+    return amount
